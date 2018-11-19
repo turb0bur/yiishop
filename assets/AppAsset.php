c05c30ab5eed0fd194df55226fffc65d3be3566e
Volodymyr Butko <turb0bur@ukr.net>
2018-11-19 22:48:53 +0200

    Changed main layout and home page with appropriate assets

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @link http://www.yiiframework.com/
+ * @link      http://www.yiiframework.com/
  * @copyright Copyright (c) 2008 Yii Software LLC
- * @license http://www.yiiframework.com/license/
+ * @license   http://www.yiiframework.com/license/
  */
 
 namespace app\assets;
@@ -11,19 +11,31 @@ use yii\web\AssetBundle;
 
 /**
  * @author Qiang Xue <qiang.xue@gmail.com>
- * @since 2.0
+ * @since  2.0
  */
 class AppAsset extends AssetBundle
 {
     public $basePath = '@webroot';
-    public $baseUrl = '@web';
-    public $css = [
-        'css/site.css',
+    public $baseUrl  = '@web';
+    public $css      = [
+//        'css/bootstrap.min.css',
+        'css/font-awesome.min.css',
+        'css/prettyPhoto.css',
+        'css/price-range.css',
+        'css/animate.css',
+        'css/main.css',
+        'css/responsive.css',
     ];
-    public $js = [
+    public $js       = [
+//        'js/jquery.js',
+//        'js/bootstrap.min.js',
+        'js/jquery.scrollUp.min.js',
+        'js/price-range.js',
+        'js/jquery.prettyPhoto.js',
+        'js/main.js',
     ];
-    public $depends = [
+    public $depends  = [
         'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
+        'yii\bootstrap\BootstrapPluginAsset',
     ];
 }
