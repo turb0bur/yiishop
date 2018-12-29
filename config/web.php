e1b4e4f1b0900aae3c7a268c0f876fe5cffdf8d7
Volodymyr Butko <turb0bur@ukr.net>
2018-12-29 19:23:02 +0200

    Added pagination to a category page

--- config/web.php
+++ config/web.php
@@ -45,7 +45,8 @@ $config = [
             'enablePrettyUrl' => true,
             'showScriptName'  => false,
             'rules'           => [
-                'category/<id:\d+>' => 'category/view'
+                'category/<id:\d+>/page/<page:\d+>' => 'category/view',
+                'category/<id:\d+>'                 => 'category/view'
             ],
         ],
 
